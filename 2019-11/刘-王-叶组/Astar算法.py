4cd392e9b90dc8c2eb43ee696c4aba2cd465adc2
lichye <50267700+lichye@users.noreply.github.com>
2019-12-13 11:45:41 +0800

    写了一部分代码

---
+++ 2019-11/刘-王-叶组/Astar算法.py
@@ -0,0 +1,158 @@
+import math
+#本地图使用一个装着点的列表来表示整张地图
+points=[]
+
+tree=[]#用这个列表装最终的查询结果
+
+#运动方向的元组
+direct=([1,0],[0,1],[0,-1],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1])
+
+#初始化地图函数
+def ini(points,x,y):
+    for i in range(1,x+1):
+        for j in range(1,y+1):
+            new_point={'x':i,'y':j,'value':1}
+            points.append(new_point)
+            
+#设置地图障碍物的函数
+def setbound(points,x,y):
+    for point in points:
+        if point['x']==x and point['y']==y:
+            point['value']=0
+
+#查询某个点的周围的点
+def checkpoint(Bpoints,x,y,Apoints):
+    global direct
+    centerpoint={'x':x,'y':y,'value':1}
+    if centerpoint in Bpoints:
+        for i in range(0,8):#遍历8个方向
+            ex=x+direct[i][0]
+            ey=y+direct[i][1]
+            temp={'x':ex,'y':ey,'value':1}
+            if temp in Bpoints:#如果这个方向有这个点，并且填入
+                    insert(x,y,ex,ey,Apoints)
+
+#将某个点插入到Apoint当中去
+def insert(x,y,ex,ey,Apoints):
+    global endx
+    global endy
+    newpoint={'x':ex,'y':ey,'H':math.sqrt(abs((x-ex)*10.0)**2+abs((y-ex)*10.0)**2),'P':abs(ex-endx)*10.0+abs(ey-endy)*10.0}
+    if len(Apoint)==0:
+        Apoints.append(newpoint)
+    fla=0
+    for i in range(0,len(Apoints)):
+        if(Apoints[i]['x']==ex and Apoints[i]['y']==ey)#如果存在这个点的话
+            fla=1#不需要插入
+            if(math.sqrt(abs((x-ex)*10.0)**2+abs((y-ex)*10.0)**2)+abs(ex-endx)*10.0+abs(ey-endy)*10.0<Apoints[i]['H']+Apoints[i]['P']):
+                   Apoints[i]['H']=math.sqrt(abs((x-ex)*10.0)**2+abs((y-ex)*10.0)**2)
+                   Apoints[i]['P']=abs(ex-endx)*10.0+abs(ey-endy)*10.0
+    
+    if(fla==0):#如果没找到
+        Apoints.append(newpoint)
+                   
+   
+    
+#输入地图的宽度和长度
+x=input('请输入地图宽度')
+x=int(x)
+y=input('请输入地图长度')
+y=int(y)
+
+#初始化地图
+ini(points,x,y)
+
+#输入障碍点
+while 1:
+    print('请输入障碍点，如果想结束输入，则xy都为-1')
+    xi=input('输入障碍点的x')
+    xi=int(xi)
+    yi=input('输入障碍点的y')
+    yi=int(yi)
+    if xi==-1 and yi==-1:
+        break
+    setbound(points,xi,yi)#将那个点设置为障碍物
+
+#输入起点
+startx=input('请输入你的起点的x')
+starty=input('请输入你的起点的y')
+startx=int(startx)
+starty=int(starty)
+
+#输入终点
+endx=input('请输入你的终点的x')
+endy=input('请输入你的终点的y')
+endx=int(endx)
+endy=int(endy)
+
+#表示已经搜索过的点集
+Apoints=[]
+
+#表示等待搜索的点集
+Bpoints=points
+flag=1
+
+
+checkpoint(Bpoints,startx,starty,Apoints)#查询第一个点
+setbound(Bpoints,startx,starty)
+lastx=startx
+lasty=starty
+
+end_point={'x':endx,'y':endy,'value':1}
+
+while end_point in Bpoints:#如果终点还没有被走过,就进行循环
+    print('dosomething')
+    if len(Apoints)==0:#如果没有带测点，就结束
+        flag=0
+        break
+
+    #默认最好的点是Apoint中的第一个
+    minH=Apoints[0]['H']+Apoints[0]['P']
+    minx=Apoints[0]['x']
+    miny=Apoints[0]['y']
+
+    for point in Apoints:#遍历所有的A中的点，找出最好的点
+        if minH<point['H']+point['H']:
+            minx=point['x']
+            miny=point['y']
+            minH=point['H']+point['P']
+    
+    #从A中去掉这个点
+    cnt=len(Apoints)
+    for i in range(0,cnt):
+        if Apoints[i]['x']==minx and Apoints[i]['y']==miny and Apoints[i]['H']+Apoints[i]['P']==minH:
+            del Apoints[i]
+            break
+        
+    checkpoint(Bpoints,minx,miny,Apoints)#查询最优点的周围的点
+
+    #接着把minx,miny从B点去掉,并且令这个点的父节点为上一次的最佳节点
+    setbound(Bpoints,minx,miny)
+    
+    new_point={'x':minx,'y':miny,'fx':lastx,'fy':lasty}
+    las=0
+    if(len(tree)==0):
+        tree.append(new_point)
+        las=1
+    else:
+        for i in range(0,len(tree)):
+            if(tree[i]['x']==minx and tree[i]['y']==miny):
+                #print('进行了覆盖')
+                tree[i]['fx']=lastx
+                tree[i]['fy']=lasty
+                las=1
+                break
+    if(las==0):
+        tree.append(new_point)
+    lastx=minx
+    lasty=miny
+    
+        
+        
+if flag==0:
+    print('并不存在路径')
+for i in range(1,len(tree)):
+    if tree[i]['x']==4 and tree[i]['y']==4:
+        print(tree[i])
+
+print(tree)
+print('成功运行')
---
+++ 2019-11/刘-王-叶组/树莓派的主代码.py
@@ -0,0 +1,134 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+#引入所有的针脚
+PWMA = 18
+AIN1   =  22
+AIN2   =  27
+
+PWMB = 23
+BIN1   = 25
+BIN2  =  24
+
+m=0
+n=-1
+
+def sensorCallback(channel):#感知
+    global n
+    if GPIO.input(channel):
+        n=n+1
+        return n*21.9915/5
+    else:
+        n=n+1
+        return n*21.9915/5
+
+def t_up(speed,t_time):
+        L_Motor.ChangeDutyCycle(speed)
+        GPIO.output(AIN2,False)#AIN2
+        GPIO.output(AIN1,True) #AIN1
+
+        R_Motor.ChangeDutyCycle(speed)
+        GPIO.output(BIN2,False)#BIN2
+        GPIO.output(BIN1,True) #BIN1
+        time.sleep(t_time)
+        
+def t_stop():
+        L_Motor.ChangeDutyCycle(0)
+        GPIO.output(AIN2,False)#AIN2
+        GPIO.output(AIN1,False) #AIN1
+
+        R_Motor.ChangeDutyCycle(0)
+        GPIO.output(BIN2,False)#BIN2
+        GPIO.output(BIN1,False) #BIN1
+        
+def t_down(speed,t_time):
+        L_Motor.ChangeDutyCycle(speed)
+        GPIO.output(AIN2,True)#AIN2
+        GPIO.output(AIN1,False) #AIN1
+
+        R_Motor.ChangeDutyCycle(speed)
+        GPIO.output(BIN2,True)#BIN2
+        GPIO.output(BIN1,False) #BIN1
+        time.sleep(t_time)
+
+def t_left(speed,t_time):
+        L_Motor.ChangeDutyCycle(speed)
+        GPIO.output(AIN2,True)#AIN2
+        GPIO.output(AIN1,False) #AIN1
+
+        R_Motor.ChangeDutyCycle(speed)
+        GPIO.output(BIN2,False)#BIN2
+        GPIO.output(BIN1,True) #BIN1
+        time.sleep(t_time)
+
+def t_right(speed,t_time):
+        L_Motor.ChangeDutyCycle(speed)
+        GPIO.output(AIN2,False)#AIN2
+        GPIO.output(AIN1,True) #AIN1
+
+        R_Motor.ChangeDutyCycle(speed)
+        GPIO.output(BIN2,True)#BIN2
+        GPIO.output(BIN1,False) #BIN1
+        time.sleep(t_time)    
+
+def begin():
+    GPIO.setwarnings(False) 
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(AIN2,GPIO.OUT)
+    GPIO.setup(AIN1,GPIO.OUT)
+    GPIO.setup(PWMA,GPIO.OUT)
+
+    GPIO.setup(BIN1,GPIO.OUT)
+    GPIO.setup(BIN2,GPIO.OUT)
+    GPIO.setup(PWMB,GPIO.OUT)
+
+    L_Motor= GPIO.PWM(PWMA,100)
+    L_Motor.start(0)
+
+    R_Motor = GPIO.PWM(PWMB,100)
+    R_Motor.start(0)
+        
+    GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(17,GPIO.BOTH,callback=sensorCallback,bouncetime=200)
+
+def moveon(speed,lenth):
+    L_Motor.ChangeDutyCycle(speed)
+    GPIO.output(AIN2,False)#AIN2
+    GPIO.output(AIN1,True) #AIN1
+
+    R_Motor.ChangeDutyCycle(speed)
+    GPIO.output(BIN2,False)#BIN2
+    GPIO.output(BIN1,True) #BIN1
+    while sensorCallback()<lenth:
+        time.sleep(0.01)
+        
+    L_Motor.ChangeDutyCycle(0)
+    GPIO.output(AIN2,False)#AIN2
+    GPIO.output(AIN1,False) #AIN1
+
+    R_Motor.ChangeDutyCycle(0)
+    GPIO.output(BIN2,False)#BIN2
+    GPIO.output(BIN1,False) #BIN1
+    
+def nextline(speed,lenth):
+    L_Motor.ChangeDutyCycle(speed)
+    GPIO.output(AIN2,False)#AIN2
+    GPIO.output(AIN1,True) #AIN1
+
+    R_Motor.ChangeDutyCycle(speed)
+    GPIO.output(BIN2,True)#BIN2
+    GPIO.output(BIN1,False) #BIN1
+    while sensorCallback()<lenth:
+        time.sleep(0.01)
+     L_Motor.ChangeDutyCycle(0)
+    GPIO.output(AIN2,False)#AIN2
+    GPIO.output(AIN1,False) #AIN1
+
+    R_Motor.ChangeDutyCycle(0)
+    GPIO.output(BIN2,False)#BIN2
+    GPIO.output(BIN1,False) #BIN1
+
+
+#使用字典来完成地图的创建
+    
